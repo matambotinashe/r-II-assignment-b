470eb2145e2afe06a3dbdaa68d3a509de8aae614
Tinashe Matambo <39901158+matambotinashe@users.noreply.github.com>
2018-12-28 00:00:49 +0200

    Add files via upload

---
+++ assignment2b-R revised.R
@@ -0,0 +1,29 @@
+
+M=10
+N=10
+set.seed(Sys.time())
+matrix = matrix( rnorm(N), N, M)
+df = data.frame(matrix)
+
+#vectorized
+df1 = sin(df) + df 
+
+
+
+for (i in 1:N){
+  for (j in 1:M){
+    df[i,j] =df[i,j] + sin(df[i,j])
+  }
+}
+df
+
+df-df1
+
+# System time difference
+system.time(sin(df) + df)
+
+system.time(for (i in 1:N){
+  for (j in 1:M){
+    df[i,j] =df[i,j] + sin(df[i,j])
+  }
+})
